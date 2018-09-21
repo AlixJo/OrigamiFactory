5cfb5dc0ab9b9d3c1a5520aef36c80165b3fdc8a
AlixJo <alix-jourdan@hotmail.fr>
2018-09-21 16:05:31 +0200

    html origami début

--- origami-angular/src/app/origami/origami.component.ts
+++ origami-angular/src/app/origami/origami.component.ts
@@ -4,18 +4,42 @@ import {OrigamiService} from './origami.service';
 import {CategorieService} from '../categorie/categorie.service';
 
 @Component({
-  templateUrl: './origami.component.html'
+  templateUrl: './origami.component.html',
+  styleUrls: ['./origami.component.css']
 })
 
 export class OrigamiComponent implements OnInit {
 
-  constructor(private origamiService: OrigamiService) { }
+  private formOrigami: Origami = null;
+
+  constructor(private origamiService: OrigamiService) {
+  }
 
   ngOnInit() {
   }
 
   public list() {
     return this.origamiService.findAll();
+  }
+
+  public add() {
+    this.formOrigami = new Origami();
+  }
+
+  public edit(id: number) {
+    this.formOrigami = this.origamiService.findById(id);
+  }
+
+  public save() {
+    this.origamiService.save(this.formOrigami);
+  }
+
+  public delete(id: number) {
+    this.origamiService.delete(this.origamiService.findById(id));
+  }
+
+  public cancel() {
+    this.formOrigami = null;
   }
 
 }
--- origami-angular/src/app/origami/origami.service.ts
+++ origami-angular/src/app/origami/origami.service.ts
@@ -20,7 +20,57 @@ export class OrigamiService {
 
   public findAll() {
     return this.origamis;
+  }
 
+  public findById(id: number, details?: boolean): any {
+    // if (details) {
+    //  return this.http
+    //    .get(this.apiUrl + id)
+    //    .subscribe(resp => resp.json());
+
+    for (const origami of this.origamis) {
+      if (origami.id === id) {
+        return origami;
+      }
+    }
+    return null;
+  }
+
+
+  public save(origami: Origami) {
+    if (origami) {
+      if (!origami.id) {
+        if (this.origamis.length > 0) {
+          origami.id = this.origamis[this.origamis.length - 1].id + 1;
+        } else {
+          origami.id = 1;
+        }
+
+        this.http
+          .post(this.apiUrl, origami)
+          .subscribe(
+            resp => this.origamis.push(origami),
+            err => console.log(err)
+          );
+      }
+      this.http
+        .put(this.apiUrl + origami.id, origami)
+        .subscribe(
+          resp => null,
+          err => console.log(err)
+        );
+    }
+  }
+
+  public delete(origami: Origami) {
+    const pos: number = this.origamis.indexOf(origami);
+
+    this.http
+      .delete(this.apiUrl + origami.id)
+      .subscribe(
+        resp => this.origamis.splice(pos, 1),
+        err => console.log(err)
+      );
   }
 
 }
